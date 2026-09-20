1775e91a68bf5b5fc198c2c1768b862966c59ad5
DivisionCero <peter.vargasg@gmail.com>
2026-09-19 19:44:30 -0500

    Feature/update more tools (#16)
    * update tools
    
    * hotfix navbar

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -155,8 +155,10 @@ export const baseOptions: BaseLayoutProps = {
           rel="noopener noreferrer"
           className={navLinkClassName}
         >
-          <UserIcon />
-          <span className="bg-fd-primary hover:bg-fd-primary/90 dark:bg-fd-primary/80 dark:hover:bg-fd-primary text-fd-primary-foreground rounded-md px-3 py-1 font-bold block md:inline-block">Regístrate</span>
+          <span className="bg-fd-primary hover:bg-fd-primary/90 dark:bg-fd-primary/80 dark:hover:bg-fd-primary text-fd-primary-foreground rounded-md px-3 py-1 font-bold flex items-center gap-1">
+            <UserIcon className="size-4" />
+            Regístrate
+          </span>
         </Link>
       ),
     },
